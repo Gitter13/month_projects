1384d380f6e63b4aeefe51267738262cda788179
Gitter13 <anna.voracova.2009@gmail.com>
2025-08-17 17:37:14 +0200

    přidaný panacci do seznamu a naprogramované i přidání textur

--- chess.cpp
+++ chess.cpp
@@ -13,10 +13,10 @@ class Panacek{
 public:
     Vector2 pozice;
     Texture2D textura;
-    char barva;
-    Panacek(Vector2 position, char ibarva, Texture2D itextura){
+    string typ;
+    Panacek(Vector2 position, string ityp, Texture2D itextura){
         pozice = position;
-        barva = ibarva;
+        typ = ityp;
         textura = itextura;
     }
 };
@@ -45,17 +45,47 @@ void vykresli_hraci_plochu(){
     }
 }
 
+Texture2D vez;
+Texture2D kun;
+Texture2D strelec;
+Texture2D kral;
+Texture2D kralovna;
+Texture2D pesek;
 void nacti_textury(){
-    //
+    vez = LoadTexture("vez.png");
+    kun = LoadTexture("kun.png");
+    strelec = LoadTexture("strelec.png");
+    kral = LoadTexture("kral.png");
+    kralovna = LoadTexture("kralovna.png");
+    pesek = LoadTexture("pesek.png");
 }
 int main(){
     InitWindow(width,height, "sachy");
     for (int s=0; s <16;s++){
         bily_bit.set(s);
-        
+        Vector2 pos = {sirka_pole/2+sirka_pole*(s%8), sirka_pole/2+int(s/8)*sirka_pole};
+        Texture2D akt_tex;
+        string akt_typ;
+        if (s == 0 || s == 7) akt_tex=vez, akt_typ = "vez";
+        if (s == 1 || s == 6) akt_tex=kun, akt_typ = "kun";
+        if (s == 2 || s == 5) akt_tex=strelec, akt_typ = "strelec";
+        if (s == 4) akt_tex=kralovna, akt_typ = "kralovna";
+        if (s == 3) akt_tex=kral, akt_typ = "kral";
+        if (s > 7) akt_tex=pesek, akt_typ = "pesek";
+        panacci.push_back(Panacek(pos,akt_typ,akt_tex));
     }
     for (int z=0; z <16;z++){
         cerny_bit.set(63-z);
+        Vector2 pos = {sirka_pole/2+sirka_pole*(z%8), height-(sirka_pole/2+int(z/8)*sirka_pole)};
+        Texture2D akt_tex;
+        string akt_typ;
+        if (z == 0 || z == 7) akt_tex=vez, akt_typ = "vez";
+        if (z == 1 || z == 6) akt_tex=kun, akt_typ = "kun";
+        if (z == 2 || z == 5) akt_tex=strelec, akt_typ = "strelec";
+        if (z == 3) akt_tex=kralovna, akt_typ = "kralovna";
+        if (z == 4) akt_tex=kral, akt_typ = "kral";
+        if (z > 7) akt_tex=pesek, akt_typ = "pesek";
+        panacci.push_back(Panacek(pos,akt_typ,akt_tex));
     }
     nacti_textury();
     while (!WindowShouldClose()){
